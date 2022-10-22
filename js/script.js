153750b39ca2d7e8fcb31239250d19950c2fd23f
Nicole <113395278+nicolep908@users.noreply.github.com>
2022-10-21 18:06:41 -0700

    repo list added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 //div class overview = profile info
 const overview = document.querySelector(".overview");
 const username = "nicolep908";
+const repoList = document.querySelector(".repo-list");
 
 const gitUserInfo = async function () {
     const userInfo = await fetch (
@@ -28,4 +29,21 @@ const displayUserInfo = function (data) {
     `;
 
 overview.append(div);
+gitRepoList();
 };
+
+const gitRepoList = async function () {
+    const gitRepo = await fetch (`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await gitRepo.json();
+    displayRepos(repoData);
+};
+
+const displayRepos = async function (repos) {
+    for (const repo of repos) {
+        const repoItem = document.createElement("li");
+        repoItem.classList.add("repo");
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(repoItem);
+     }
+};
+
